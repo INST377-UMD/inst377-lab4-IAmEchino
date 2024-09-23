b9736405bdeb873eb2100e1925141f6185a9e716
Josiah Altschuler <64720141+IAmEchino@users.noreply.github.com>
2024-09-22 21:37:45 -0400

    Code that's not working

--- functions.js
+++ functions.js
@@ -1,13 +1,34 @@
 
+// function changeColor() {
+//     alert("TEST!")
+//     // const royalblue = "rgb(65, 105, 225)"
+//     // var backgroundColor = window.getComputedStyle(document.body).backgroundColor;
+//     // console.log(backgroundColor);
+
+//     if (document.body.style.backgroundColor == "black") {
+//         document.body.style.backgroundColor = "royalblue";
+//     }
+//     if (document.body.style.backgroundColor == "royalblue") {
+//         document.body.style.backgroundColor = "black";
+//     }
+// }
+
+
+
 function changeColor() {
     const royalblue = rgb(65, 105, 225)
+    const black = rgb(0, 0, 0)
+
     // Get the computed background color of the body element
     var backgroundColor = window.getComputedStyle(document.body).backgroundColor;
-    console.log(backgroundColor);
+    console.log(window.getComputedStyle(document.body).backgroundColor);
 
-    // if (document.body.style.backgroundColor == "black") {
-    //     document.body.style.backgroundColor = "royalblue";
-    // } else if (document.body.style.backgroundColor == "royalblue") {
-    //     document.body.style.backgroundColor = "black";
-    // }
+    
+
+    if (window.getComputedStyle(document.body).backgroundColor == black) {
+        document.body.style.backgroundColor = royalblue;
+    }
+    if (window.getComputedStyle(document.body).backgroundColor == royalblue) {
+        document.body.style.backgroundColor = black;
+    }
 }
